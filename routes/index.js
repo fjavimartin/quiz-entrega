dfd1c0eab9319fd3886ae2030a15e76daa82ec5e
fjmlillo <javier.martin@e-quipos.es>
2015-06-18 20:44:58 +0200

    Añado modificación Autoload

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quizID
+router.param('quizID', quizcontroller.load);
+
 router.get('/quizes', quizcontroller.index);
 router.get('/quizes/:quizID(\\d+)', quizcontroller.show);
 router.get('/quizes/:quizID(\\d+)/answer', quizcontroller.answer);
